90e7f3cc08aeb5f132b16892a7f5402f0180859d
José Mafra <102157937+OCupido@users.noreply.github.com>
2023-08-02 09:27:37 -0300

    Add files via upload

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Ahri'},
+  { id: 13, name: 'Zoe'},
+  { id: 14, name: 'Miss Fortune'},
+  { id: 15, name: 'Nunu e Willump'},
+  { id: 16, name: 'Nami'},
+  { id: 17, name: 'Irelia'},
+  { id: 18, name: 'Lillia'},
+  { id: 19, name: 'Teemo'},
+  { id: 20, name: 'Seraphine'}
+];
